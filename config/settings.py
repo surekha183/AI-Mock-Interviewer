c24828a2353f3ffd6faab9a7937b4caaa838f99a
surekha183 <surekhavaitla183@gmail.com>
2026-08-03 19:55:48 +0530

    Configure Gmail SMTP

--- config/settings.py
+++ config/settings.py
@@ -163,7 +163,18 @@ MEDIA_URL = "/media/"
 
 MEDIA_ROOT = BASE_DIR / "media"
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
 
 AUTHENTICATION_BACKENDS = [
     "users.backends.EmailBackend",
